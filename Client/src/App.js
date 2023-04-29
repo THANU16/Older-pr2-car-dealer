fd13b150275dd5838c4ead31a1db9d0768e68479
Shan Rathsarana <91466480+shanrathsaranadata@users.noreply.github.com>
2023-04-29 16:24:16 +0530

    package update

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Leads_avalible from "./page/Leads_avalible";
 import Leads_success from "./page/Leads_success";
-import Leads_reject from "./page/Leads_reject";
+// import Leads_reject from "./page/Leads_reject";
 
 
 function App() {
